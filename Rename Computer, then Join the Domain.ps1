d4ea9f4c7140d2b7bb1dd31b9ec04d059b156abc
Kevin Skeen II <42982839+dskeen96@users.noreply.github.com>
2021-05-06 15:06:55 -0400

    Add files via upload

---
+++ Rename Computer, then Join the Domain.ps1
@@ -0,0 +1,5 @@
+﻿$hostname = read-host 'COMPUTER NAME :   '
+$Domain = 'domain.com' 
+$Credential = Get-Credential
+Rename-Computer $hostname
+Add-Computer -Domain $Domain -NewName $hostname -Credential $Credential -Restart -Force
